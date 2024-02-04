73fa96f3af790fc1235f69c8c70157886233f948
Roman Likhomanov <romanlee87@gmail.com>
2024-02-04 13:38:34 +0300

    тесты loginByUsername, loginSlice, selectors

---
+++ src/features/AuthByUserName/model/slice/loginSlice.test.ts
@@ -0,0 +1,18 @@
+import { DeepPartial } from '@reduxjs/toolkit';
+import { StateSchema } from 'app/providers/StoreProvider';
+import { loginActions, loginReducer } from 'features/AuthByUserName/model/slice/loginSlice';
+import { LoginSchema } from 'features/AuthByUserName';
+
+describe('loginSlice.test', () => {
+    test('test set username', () => {
+        const state: DeepPartial<LoginSchema> = { username: 'admin' };
+        expect(loginReducer(state as LoginSchema, loginActions.setUsername('123')))
+            .toEqual({ username: '123' });
+    });
+
+    test('test set password', () => {
+        const state: DeepPartial<LoginSchema> = { password: 'admin' };
+        expect(loginReducer(state as LoginSchema, loginActions.setPassword('123')))
+            .toEqual({ password: '123' });
+    });
+});
